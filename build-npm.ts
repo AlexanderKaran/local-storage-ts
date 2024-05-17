159e6c0b31c4dfef83d9007f9afeab1bf6f72005
Alexander Karan <tech@alexanderkaran.com>
2024-05-17 06:30:10 +0100

    Added pipeline to do first release via npm and jsr with versioning

---
+++ build-npm.ts
@@ -0,0 +1,44 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.28.0/mod.ts";
+
+const outDir = "./build/npm";
+
+await emptyDir(outDir);
+
+let [version] = Deno.args;
+if (!version) {
+  throw new Error("a version argument is required to build the npm package");
+}
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir,
+  shims: {
+    deno: false,
+  },
+  test: false,
+  typeCheck: false,
+  compilerOptions: {
+    target: "ES2020",
+    sourceMap: true,
+  },
+  package: {
+    // package.json properties
+    name: "local-storage-typescript",
+    version,
+    description: "Graph Generator",
+    license: "MIT",
+    repository: {
+      author: "tech@alexanderkaran.com",
+      type: "git",
+      url: "git+https://github.com/AlexanderKaran/local-storage-ts.git",
+    },
+    bugs: {
+      url: "https://github.com/AlexanderKaran/local-storage-ts/issues",
+    },
+    engines: {
+      node: ">= 16",
+    },
+  },
+});
+
+await Deno.copyFile("README.md", `${outDir}/README.md`);
